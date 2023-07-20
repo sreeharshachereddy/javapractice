4e144cc7380d03cfc120ffaaeb57e2f2ce9cf453
Harsha <chereadydev@gmail.com>
2023-07-20 13:49:31 -0700

    checking

---
+++ src/sectionsixcontrolflow/GreatestCommonDivisor.java
@@ -0,0 +1,19 @@
+package sectionsixcontrolflow;
+
+public class GreatestCommonDivisor {
+    public static int getGreatestCommonDivisor(int first, int second){
+        if(first <10||second < 10){
+            return -1;
+        }
+        while(first != second){
+            if(first > second){
+                first =first - second;
+            }
+            else {
+                second=second - first;
+            }
+
+        }
+        return first;
+    }
+}
