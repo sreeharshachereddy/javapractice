c22946752620d5ff5ac0ac84fed7d40eaa3071d7
Harsha <chereadydev@gmail.com>
2023-07-30 22:21:11 -0700

    checkout

--- src/sectionsixcontrolflow/DiagonalStar.java
+++ src/sectionsixcontrolflow/DiagonalStar.java
@@ -69,4 +69,25 @@ package sectionsixcontrolflow;
 //NOTE: Do not add a main method to the solution code.
 // */
 public class DiagonalStar {
+    public static void main(String[] args) {
+        printSquareStar(5);
+    }
+
+    public static void printSquareStar(int number) {
+        if (number < 5) {
+            System.out.println("Invalid Value");
+        } else {
+            for (int row = 1; row <= number; row++) {
+                for (int column = 1; column <= number; column++) {
+                    if (row == 1 || row == number || column == 1 || column == number || column == row || column == number - row + 1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println("");
+            }
+        }
+    }
 }
+
